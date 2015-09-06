953e529318b80f18675bf8947771909b333482d3
Chad Furman <chad@chadfurman.com>
2015-09-05 22:17:55 -0400

    added code for token policy

---
+++ api/policies/token.js
@@ -0,0 +1,19 @@
+module.exports = function (req, res, next) {
+  var accessToken = req.header('X-Bearer-Token');
+  sails.log('token policy');
+
+  if (!accessToken) {
+    sails.log('Access token not provided');
+    return res.json(false);
+  }
+
+  User.findOne({accessToken: accessToken}, function(err, user) {
+    if (err || !user) {
+      sails.log('Invalid auth');
+      return res.json(false);
+    } else {
+      sails.log('token policy passed');
+      next();
+    }
+  });
+};
